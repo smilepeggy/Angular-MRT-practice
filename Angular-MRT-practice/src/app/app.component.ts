2102676c8e5abe1b914d6dfeeb86281a4f765d7a
Peggy <peggy22509385@gmail.com>
2020-05-19 10:10:33 +0200

    use ngFor and {{}}

--- Angular-MRT-practice/src/app/app.component.ts
+++ Angular-MRT-practice/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// Constant
+import { stationList } from './station-list.const';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-MRT-practice';
+  list = stationList;
 }
---
+++ Angular-MRT-practice/src/app/station-list.const.ts
@@ -0,0 +1,110 @@
+export const stationList = [
+
+  {
+    'Station': '大湖公園站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '大湖公園站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '中山站',
+    'Destination': '大安站',
+    'UpdateTime': '2020-05-19T15:52:55.533'
+  },
+  {
+    'Station': '中山國中站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '木柵站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '永安市場站',
+    'Destination': '蘆洲站',
+    'UpdateTime': '2020-05-19T15:52:51.1'
+  },
+  {
+    'Station': '行天宮站',
+    'Destination': '南勢角站',
+    'UpdateTime': '2020-05-19T15:52:59.92'
+  },
+  {
+    'Station': '行天宮站',
+    'Destination': '蘆洲站',
+    'UpdateTime': '2020-05-19T15:53:01.233'
+  },
+  {
+    'Station': '府中站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:47.093'
+  },
+  {
+    'Station': '忠孝復興站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:57.04'
+  },
+  {
+    'Station': '東湖站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '東湖站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '科技大樓站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '迴龍站',
+    'Destination': '南勢角站',
+    'UpdateTime': '2020-05-19T15:52:39.99'
+  },
+  {
+    'Station': '國父紀念館站',
+    'Destination': '頂埔站',
+    'UpdateTime': '2020-05-19T15:52:57.04'
+  },
+  {
+    'Station': '善導寺站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:57.04'
+  },
+  {
+    'Station': '新埔站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:47.093'
+  },
+  {
+    'Station': '劍南路站',
+    'Destination': '南港展覽館站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  },
+  {
+    'Station': '頭前庄站',
+    'Destination': '南勢角站',
+    'UpdateTime': '2020-05-19T15:52:59.92'
+  },
+  {
+    'Station': '雙連站',
+    'Destination': '北投站',
+    'UpdateTime': '2020-05-19T15:52:44.827'
+  },
+  {
+    'Station': '麟光站',
+    'Destination': '動物園站',
+    'UpdateTime': '2020-05-19T15:52:24'
+  }
+
+
+];
